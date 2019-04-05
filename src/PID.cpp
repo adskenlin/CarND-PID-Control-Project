c921523962bf2f0990671590f215a0d04c4d72eb
Ken Lin <42996768+adskenlin@users.noreply.github.com>
2019-04-05 22:37:43 +0200

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,17 +1,13 @@
 #include "PID.h"
 
-/**
- * TODO: Complete the PID class. You may add any additional desired functions.
- */
+
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
-  /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   */
+
   Kp = Kp_;
   Ki = Ki_;
   Kd = Kd_;
@@ -19,9 +15,7 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
 }
 
 void PID::UpdateError(double cte) {
-  /**
-   * TODO: Update PID errors based on cte.
-   */
+
   p_error = cte;
   
   d_error = (cte-p_error);
@@ -31,8 +25,6 @@ void PID::UpdateError(double cte) {
 }
 
 double PID::TotalError() {
-  /**
-   * TODO: Calculate and return the total error
-   */
-  return -Kp*p_error - Kd*d_error - Ki*i_error;  // TODO: Add your total error calc here!
+
+  return -Kp*p_error - Kd*d_error - Ki*i_error;
 }
